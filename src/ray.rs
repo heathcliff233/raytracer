fc42fbd3bd8d7bcd0d4ea8d72a4461e6855f564f
qqqqxxx <tao_qingxiao@sjtu.edu.cn>
2020-07-23 11:33:55 +0800

    天天摸鱼，我就是工科拖后腿第一名

--- src/ray.rs
+++ src/ray.rs
@@ -1,5 +1,14 @@
 use crate::vec3::{Color, Point3, Vec3};
 
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+    let oc = r.orig - *center;
+    let a = r.dir * r.dir;
+    let b = oc * r.dir * 2.0;
+    let c = oc * oc - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
+}
+
 #[derive(Clone, Debug, PartialEq)]
 pub struct Ray {
     pub orig: Point3,
@@ -12,6 +21,21 @@ impl Ray {
     }
 
     pub fn ray_color(&self) -> Color {
+        if hit_sphere(
+            &Point3 {
+                x: 0.0,
+                y: 0.0,
+                z: -1.0,
+            },
+            0.5,
+            &self,
+        ) {
+            return Color {
+                x: 255.0,
+                y: 0.0,
+                z: 0.0,
+            };
+        }
         let dirc: Vec3 = self.dir.unit();
         let t = 0.5 * (dirc.y + 1.0);
         Color::ones() * 255.0 * (1.0 - t)
