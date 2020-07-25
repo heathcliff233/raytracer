d625e905883e3f25927ee184a6163b3d379f219e
qqqqxxx <tao_qingxiao@sjtu.edu.cn>
2020-07-26 03:04:43 +0800

    12 finished

--- src/camera.rs
+++ src/camera.rs
@@ -1,7 +1,7 @@
 use crate::ray::Ray;
 use crate::{
     rtweekend::degrees_to_radians,
-    vec3::{Point3, Vec3},
+    vec3::{random_in_unit_disk, Point3, Vec3},
 };
 
 #[derive(Clone, Debug)]
@@ -10,29 +10,52 @@ pub struct Camera {
     lower_left_corner: Point3,
     horizontal: Vec3,
     vertical: Vec3,
+    u: Vec3,
+    v: Vec3,
+    w: Vec3,
+    lens_radius: f64,
 }
 
 impl Camera {
-    pub fn new(lookfrom: Point3, lookat: Point3, vup: Vec3, vfov: f64, aspect_ratio: f64) -> Self {
+    pub fn new(
+        lookfrom: Point3,
+        lookat: Point3,
+        vup: Vec3,
+        vfov: f64,
+        aspect_ratio: f64,
+        aperture: f64,
+        focus_dist: f64,
+    ) -> Self {
         let theta = degrees_to_radians(vfov);
         let h = (theta / 2.0).tan();
         let viewport_height = 2.0 * h;
         let viewport_width = aspect_ratio * viewport_height;
-        let w = (lookfrom - lookat).unit();
-        let u = vup.cross(w).unit();
-        let v = w.cross(u);
+        let ww = (lookfrom - lookat).unit();
+        let uu = vup.cross(ww).unit();
+        let vv = ww.cross(uu);
         Self {
             origin: lookfrom,
-            horizontal: u * viewport_width,
-            vertical: v * viewport_height,
-            lower_left_corner: lookfrom - u / 2.0 * viewport_width - v / 2.0 * viewport_height - w,
+            horizontal: uu * viewport_width * focus_dist,
+            vertical: vv * viewport_height * focus_dist,
+            lower_left_corner: lookfrom
+                - uu / 2.0 * viewport_width * focus_dist
+                - vv / 2.0 * viewport_height * focus_dist
+                - ww * focus_dist,
+            u: uu,
+            v: vv,
+            w: ww,
+            lens_radius: aperture / 2.0,
         }
     }
 
     pub fn get_ray(&self, s: f64, t: f64) -> Ray {
+        let rd = random_in_unit_disk() * self.lens_radius;
+        let offset = self.u * rd.x + self.v * rd.y;
         Ray {
-            orig: self.origin,
-            dir: self.lower_left_corner + self.horizontal * s + self.vertical * t - self.origin,
+            orig: self.origin + offset,
+            dir: self.lower_left_corner + self.horizontal * s + self.vertical * t
+                - self.origin
+                - offset,
         }
     }
 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -88,6 +88,16 @@ pub fn random_unit_vector() -> Vec3 {
     }
 }
 
+pub fn random_in_unit_disk() -> Vec3 {
+    loop {
+        let p = Vec3::new(random_double(-1.0, 1.0), random_double(-1.0, 1.0), 0.0);
+        if p.squared_length() >= 1.0 {
+            continue;
+        }
+        return p;
+    }
+}
+
 pub fn reflect(v: &Vec3, n: &Vec3) -> Vec3 {
     *v - *n * (*v * *n) * 2.0
 }
